f12e61d53297a1902002a987e5a2faac41d05025
DESKTOP-HIOHEJO\PC <310296409@qq.com>
2019-12-27 17:33:42 +0800

    修改细节、

--- src/components/dietaryManagement/FoodType.vue
+++ src/components/dietaryManagement/FoodType.vue
@@ -10,7 +10,7 @@
         </el-col>
         <!-- 添加用户按钮 -->
         <el-col :span="4">
-          <el-button type="primary" @click="addWater">新增估量食物类型</el-button>
+          <el-button type="primary" @click="addWater">新增食物类型</el-button>
         </el-col>
       </el-row>
       <!-- 表格 -->
